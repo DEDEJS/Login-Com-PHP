5f5d77e925d9aefb87426259ad621ad26065fee1
André Aparecido <andreaparecido892@gmail.com>
2022-10-11 00:01:44 -0300

    Add files via upload

--- Cadastro.php
+++ Cadastro.php
@@ -1,6 +1,6 @@
 <?php 
 ini_set('default_charset','UTF-8');
-
+include_once "php/ValidaDados.php";
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -9,27 +9,44 @@ ini_set('default_charset','UTF-8');
   <title>Login</title>
   <meta name="viewport" content="initial-scale=1">
   <link rel="stylesheet" type="text/css" href="css/index.css">
+
 </head>
-<body>
+<body onload="return Validacao();">
+<header>
+
 <nav>
  <ul>
-  <li>Cadastrar</li>
-  <li>Logar</li>
+  <li><a href="Cadastro.php">Cadastrar</a></li>
+  <li><a href="Login.php">Logar</a></li>
  </ul>
 </nav>
-<header>
  <h1>Cadastro</h1> 
 </header>
 <main>
- <form method="POST" action="#">
-    <?php ?>
-   <input type="text" name="NomeCadastro" placeholder="Seu Nome">
-   <?php  ?>
-   <input type="text" name="EmailCadastro" placeholder="Seu Email">
-   <?php  ?>
-   <input type="password" name="SenhaCadastro" placeholder="Sua Senha">
-   <input type="submit" value="Cadastrar">
+ <form method="POST" action="#" >
+   <p id="ErrorNome">
+   <?php 
+    $ValidaInputs ->  ValidaNome($Nome);
+    ?>
+    </p>
+   <input type="text" name="Nome" id="Nome" placeholder="Seu Nome" title="Campo Nome">
+   <p id="ErrorEmail">
+   <?php  
+     $ValidaInputs -> ValidaEmail($Email);
+   ?>
+   </p>
+   <input type="text" name="Email" id="Email" placeholder="Seu Email" title="Campo Email">
+  <p id="ErrorSenha">
+  <?php  
+   $ValidaInputs -> ValidaSenha($Senha);
+   ?>
+   </p>
+   <input type="password" name="Senha" id="Senha" placeholder="Sua Senha" title="Campo Senha">
+   <input type="submit" value="Cadastrar" title="Cadastrar">
  </form>
 </main>
+
+<script type="text/javascript" src="JS/ValidaForm.js" deffer></script>
+
 </body>
 </html>
--- Login.php
+++ Login.php
@@ -1,6 +1,6 @@
 <?php 
 ini_set('default_charset','UTF-8');
-include_once "php/ValidaLogin.php";
+include_once "php/ValidaDados.php";
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -10,32 +10,35 @@ include_once "php/ValidaLogin.php";
   <meta name="viewport" content="initial-scale=1">
   <link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
-<body>
+<body onload="return Validacao();">
+<header>
 <nav>
  <ul>
-  <li><a href="Cadastrar.php" title="Cadastrar">Cadastrar</a></li>
+  <li><a href="Cadastro.php" title="Cadastrar">Cadastrar</a></li>
   <li><a href="Login.php" title="Logar">Logar</a></li>
  </ul>
 </nav>
-<header>
+
  <h1>Login</h1> 
 </header>
 <main>
  <form method="post" action="#">
-  <p>
+  <p id="ErrorEmail">
    <?php 
-    $ValidaLogin -> ValidaEmailLogin($EmailLogin);
+    $ValidaInputs -> ValidaEmail($Email);
     ?>
    </p>
-   <input type="text" name="LoginEmail" placeholder="Seu Email">
-   <p>
+   <input type="text" name="Email" id="Email" placeholder="Seu Email">
+   <p id="ErrorSenha">
     <?php 
-     $ValidaLogin -> ValidaSenhaLogin($SenhaLogin);
+     $ValidaInputs -> ValidaSenha($Senha);
     ?>
    </p>
-   <input type="password" name="LoginSenha" placeholder="Sua Senha">
+   <input type="password" name="Senha" id="Senha" placeholder="Sua Senha">
    <input type="submit" value="Logar">
  </form>
 </main>
+<script type="text/javascript" src="JS/ValidaForm.js" deffer></script>
+
 </body>
 </html>
